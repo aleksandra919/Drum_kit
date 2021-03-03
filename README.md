2f4543c96b12c49047f779df9799fc5f8a7820e9
Rolka <arrk@gft.com>
2021-03-03 15:48:35 +0100

    clean code, add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# DrumKit
+
+This is simple drum kit app. Press button on keyboard to hear sound.
+
+Technologies: JavaScript, HTML, CSS.
+
+![](drumKit.gif)
--- js/script.js
+++ js/script.js
@@ -1,33 +1,23 @@
 
-  function playSound(e) {                                                                   
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);     
-//Do stałej audio przypisz element audio posiadający atrybut "data-key" o wartości e.keycode.
-//Właściwość keyCode zawiera wartość odpowiadającą wciśniętemu klawiszowi.                                
-//zmienna const musi miec przypisana wartosc / let i const (stala-wartosc nie moze byc zmieniona) - zakres tych zmiennych to blok np instrukcja warunkowa if
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);  
-//Do stałej keys przypisz klasę key posiadający atrybut "data-key" o wartości e.keycode.
-//QuerySelector - zwraca pierwszy element ktory pasuje do podanego selektora
-    if (!audio) return;                        
-//zamyka jesli nie ma takiego kodu (klawisz nie jest zdefiniowany = nie ma przypisanej wartosci keyCode)
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+  
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+    if (!audio) return;
+
     audio.currentTime = 0;
-//funkcja sprawiająca, że nie czekamy na zakończenie dźwięku, tylko ładuje się szybko od nowa, cofa do poczatku
-    audio.play();                                                              
-//odtwarzaj audio
+    audio.play();
     key.classList.add('playing');
   }
 
-  
   function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;     //zatrzymuje funkcje                              
-// e- zmienna reprezentujaca event(zdarzenie)
-    e.target.classList.remove('playing');                                       
-//targert wskazuje na obiekt ktory wyzwolil dana funkcje
-// ALTERNATYWA :  this.classList.remove('playing');
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
   }
 
-  const keys = Array.from(document.querySelectorAll('.key'));                 
-//Array.from()  metoda tworzy nową instację tablicy z obiektu podobnego do tablicy lub obiektu iterowalnego.
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition)); //jest kilka elementow z type:"transitionend", wybieramy jeden: propertyName transition'
-//ALTERNATYWA: document.querySelector('.keys').addEventListener('transitionend', removeTransition);
-  window.addEventListener('keydown', playSound); //czeka na zdarzenei keydown i odpala funkcje playSounds
+
 
